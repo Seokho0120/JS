f75ed3f9e4dc0f8f8b03ffb46afc157635dd6f1e
Seokho <seokho0120@gmail.com>
2025-03-09 01:01:53 +0900

    feat: 1차 완성

--- Todo/main.js
+++ Todo/main.js
@@ -1,29 +1,37 @@
-// const addBtn = document.querySelector('#addBtn')
-// addBtn.addEventListener('click', () => {
-//   // items 배열에 value를 넣어주면됨
-//   if(todoInput.value !== '') {
-//     keyCodeCheck()
-//   }
-// })
+const addBtn = document.querySelector('#addBtn')
 
 function keyCodeCheck() {
-  // console.log(window.event.keyCode)
-  if(window.event.keyCode === 13 && todoInput.value !== '') {
-    const todoList = document.querySelector('#todoList')
-    const newLi = document.createElement('li')
-    const newBtn = document.createElement('button'); 
-    const newSpan = document.createElement('span');
-    const todoInput = document.querySelector('#todoInput')
-
-    newLi.appendChild(newBtn);
-    newLi.appendChild(newSpan);
-    console.log('newLi', newLi);
-
-    newSpan.textContent = todoInput.value;
-
-    todoList.appendChild(newLi);
-
-    todoInput.value = '';
+  if(window.event.keyCode === 13 && todoInput.value !== ''){
+    createTodo()
   }
+}
+
+addBtn.addEventListener('click', () => {
+  if(todoInput.value !== '') {
+    console.log('input value -->',todoInput.value)
+    createTodo()
+  }
+})
+
+function createTodo() {
+  const todoList = document.querySelector('#todoList')
+  const newLi = document.createElement('li')
+  const newBtn = document.createElement('button'); 
+  const newSpan = document.createElement('span');
+  const todoInput = document.querySelector('#todoInput')
+
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  console.log('newLi',newLi)
+
+  newSpan.textContent = todoInput.value;
+
+  todoList.appendChild(newLi);
+
+  todoInput.value = '';
+
+  newBtn.addEventListener('click', () => {
+    
+  })
 }
 
