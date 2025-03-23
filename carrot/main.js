6f0e8516dfbd8cc80bacb10904a0ff69f4c5f142
Seokho <seokho0120@gmail.com>
2025-03-23 22:00:59 +0900

    feat: 랜덤으로 이미지 필드안에서만 보여지게 수정, class 이름 수정, 리팩토링

--- carrot/main.js
+++ carrot/main.js
@@ -1,34 +1,57 @@
-const playBtn = document.querySelector(".play-btn");
-const itemList = document.querySelector(".item-list");
-const items = [];
+const playBtn = document.querySelector(".game-btn");
+const itemList = document.querySelector(".game-field");
 
-// 플레이 버튼 클릭하면 itemList에 item 10개가 생김
-// item이 생기면서 위치는 랜덤으로 매번 바뀜
 playBtn.addEventListener("click", (e) => {
   e.preventDefault();
-
-  for (let i = 0; i < 10; i++) {
-    makeItem(); // 10번 돌아야됨
-  }
+  createItems(10); // 10번 돌아야됨
 });
 
-function makeItem() {
-  const item = createItem();
+function createItems(count) {
+  for (let i = 0; i < count; i++) {
+    const item = createItem();
 
-  itemList.appendChild(item);
-  console.log("itemList", itemList);
+    itemList.appendChild(item);
+    console.log("itemList", itemList);
+  }
+  // const item = createItem();
+  // itemList.appendChild(item);
 }
 
 function createItem() {
-  const itemImage = document.createElement("li");
+  const itemImage = document.createElement("img");
   itemImage.setAttribute("class", "item-img");
 
   const random = Math.random(); // 0과 1 사이의 난수를 생성
   if (random < 0.5) {
-    itemImage.innerHTML = `<img src="./img/bug.png" alt="bug" />`;
+    itemImage.src = "./img/bug.png"; // 이미지를 직접 설정
+    itemImage.alt = "bug";
   } else {
-    itemImage.innerHTML = `<img src="./img/carrot.png" alt="carrot" />`;
+    itemImage.src = "./img/carrot.png"; // 이미지를 직접 설정
+    itemImage.alt = "carrot";
   }
+
+  // if (random < 0.5) {
+  //   itemImage.innerHTML = `<img src="./img/bug.png" alt="bug" />`;
+  // } else {
+  //   itemImage.innerHTML = `<img src="./img/carrot.png" alt="carrot" />`;
+  // }
+
+  const itemListRect = itemList.getBoundingClientRect();
+  console.log("itemListRect", itemListRect);
+
+  // 랜덤 위치 계산 (itemList 내부에만 위치하도록)
+  const randomX = Math.random() * (itemListRect.width - 100);
+  const randomY = Math.random() * (itemListRect.height - 100);
+
+  console.log("randomX", randomX);
+  console.log("randomY", randomY);
+
+  itemImage.style.position = "absolute";
+  itemImage.style.left = `${randomX}px`;
+  itemImage.style.top = `${randomY}px`;
+
+  // itemImage.style.left = "50px";
+  // itemImage.style.right = "100px";
 
   return itemImage;
 }
